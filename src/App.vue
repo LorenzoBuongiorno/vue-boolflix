cdf663dcec59c8177cde3629b72067d5cac349b2
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-02-03 12:19:47 +0100

    added axios api call

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="app">
-
+    <header>
+      <input-search />
+    </header>
+    <main>
+      <main-box />
+      <div v-for="item in movies" :key="item.id">{{item.title}}</div>
+    </main>
   </div>
 </template>
 
 <script>
+import InputSearch from './components/InputSearch.vue'
+import MainBox from './components/MainBox.vue'
+import axios from 'axios'
 
 
 export default {
   name: 'App',
   components: {
+    InputSearch,
+    MainBox
 
+  },
+  data () {
+    return {
+      movies: [],
+      keywordSearch: ''
+    }
+  },
+  methods: {
+    filterMovies(keywordSearch) {
+      axios.get(`https://api.themoviedb.org/3/search/movie?language=it-IT&api_key=3800b334d89bd3d998f99ccfbfa37575&query=${keywordSearch}`).then((element) => {
+      this.movies = element.data.results
+    })
+  }
   }
 }
 </script>
---
+++ src/components/InputSearch.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input type="text" placeholder="Search">
+        <button>Search</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
