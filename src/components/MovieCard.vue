198a116371ed6c1f84ab3f4ea00a313546bb89ed
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-02-04 16:44:44 +0100

    added vote stars

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -15,11 +15,13 @@
                 <span v-if="!isLoaded">{{movie.original_language}}</span>
             </div>
             <div>
-                <span>Voto:</span>
-                {{movie.vote_average}}
+                <span>Voto: </span>
+                <span class="stars" v-for="n in Math.ceil(scale(movie.vote_average))" :key="n"><i class="fas fa-star"></i></span>
             </div>
         </div>
-        <img class="poster-image" :src="'https://image.tmdb.org/t/p/original' + movie.poster_path " alt="">
+        <a href="#">
+            <img class="poster-image" :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path " alt="">
+        </a>
     </div>
 </template>
 
@@ -36,11 +38,17 @@ export default {
     methods: {
         loadImage() {
                 this.isLoaded = true;
-            }
+            },
+            scale (number) {
+    return (number - 0) * (5 - 0) / (10 - 0) + 0;
+            },
+
+
     }
 }
 </script>
 
 <style lang="scss">
+@import url('https://use.fontawesome.com/releases/v5.7.1/css/all.css');
 @import '@/style/MovieCards.scss'
 </style>
--- src/components/SerieCard.vue
+++ src/components/SerieCard.vue
@@ -15,11 +15,12 @@
                 <span v-if="!isLoaded">{{serie.original_language}}</span>
             </div>
             <div>
-                <span>Voto:</span>
-                {{serie.vote_average}}
+                <span>Voto: </span>
+                <span class="stars" v-for="n in Math.ceil(scale(serie.vote_average))" :key="n"><i class="fas fa-star"></i></span>
+
             </div>
         </div>
-        <img class="poster-image" :src="'https://image.tmdb.org/t/p/original' + serie.backdrop_path " alt="">
+        <a href="#"><img class="poster-image" :src="'https://image.tmdb.org/t/p/w500/' + serie.backdrop_path " alt=""></a>
     </div>
 </template>
 
@@ -36,7 +37,10 @@ export default {
     methods: {
         loadImage() {
                 this.isLoaded = true;
-            }
+            },
+            scale (number) {
+    return (number - 0) * (5 - 0) / (10 - 0) + 0;
+            },
     }
 }
 </script>
